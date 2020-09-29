567d26d2a5a2cf7d9374818aa939ffcf5c635056
Gavin <gavchan@users.noreply.github.com>
2020-09-29 16:13:05 +0800

    Rename id argument for simplicity

--- backend/drugs/schema.py
+++ backend/drugs/schema.py
@@ -5,38 +5,38 @@ from .types import ProductType, CompanyType, IngredientType
 
 class Query(graphene.ObjectType):
     all_products = graphene.List(ProductType)
-    product = graphene.Field(ProductType, product_id=graphene.Int())
+    product = graphene.Field(ProductType, id=graphene.Int())
 
     all_companies = graphene.List(CompanyType)
-    company = graphene.Field(CompanyType, company_id=graphene.Int())
+    company = graphene.Field(CompanyType, id=graphene.Int())
 
     all_ingredients = graphene.List(IngredientType)
-    ingredient = graphene.Field(IngredientType, ingredient_id=graphene.Int())
+    ingredient = graphene.Field(IngredientType, id=graphene.Int())
 
     def resolve_all_products(root, info, **kwargs):
         return Product.objects.select_related('company').all()
 
-    def resolve_product(root, info, product_id):
+    def resolve_product(root, info, id):
         try:
-            return Product.objects.get(pk=product_id)
+            return Product.objects.get(pk=id)
         except Product.DoesNotExist:
             return None
 
     def resolve_all_companies(root, info, **kwargs):
         return Company.objects.select_related('product').all()
 
-    def resolve_company(root, info, company_id):
+    def resolve_company(root, info, id):
         try:
-            return Company.objects.get(pk=company_id)
+            return Company.objects.get(pk=id)
         except Company.DoesNotExist:
             return None
 
     def resolve_all_ingredients(root, info, **kwargs):
         return Ingredient.objects.all()
 
-    def resolve_ingredient(root, info, ingredient_id):
+    def resolve_ingredient(root, info, id):
         try:
-            return Ingredient.objects.get(pk=ingredient_id)
+            return Ingredient.objects.get(pk=id)
         except Ingredient.DoesNotExist:
             return None
 
